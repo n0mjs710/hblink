f2a8fdf79b0063ff9c02db433ca96b574bd9134c
Cort Buffington <n0mjs@me.com>
2016-08-26 21:31:20 -0500

    update call routing

--- hb_routing_rules.py
+++ hb_routing_rules.py
@@ -2,7 +2,7 @@ RULES = {
     'MASTER-1': {
         'GROUP_HANGTIME': 5,
         'GROUP_VOICE': [
-            {'NAME': 'STATEWIDE', 'ACTIVE': False, 'ON': [8,], 'OFF': [9,10], 'SRC_TS': 1, 'SRC_GROUP': 1, 'DST-TYPE': 'CLIENT', 'DST_NET': 'REPEATER-1', 'DST_TS': 2, 'DST_GROUP': 2},
+            {'NAME': 'STATEWIDE', 'DST_NET': 'REPEATER-1', 'SRC_TS': 2, 'SRC_GROUP': 3120, 'DST_TS': 2, 'DST_GROUP': 3120, 'ACTIVE': True, 'TO_TYPE': 'ON', 'TIMEOUT': 2, 'ON': [8,], 'OFF': [9,10]},
             # When DMRD received on this MASTER, Time Slot 1, Talk Group 1; send to CLIENT-1 on Time Slot 2 Talk Group 2
             # This rule is NOT enabled by default
             # This rule can be enabled by transmitting on TGID 8
@@ -13,7 +13,7 @@ RULES = {
     'REPEATER-1': {
         'GROUP_HANGTIME': 5,
         'GROUP_VOICE': [
-            {'NAME': 'STATEWIDE', 'ACTIVE': False, 'ON': [8,], 'OFF': [9,10], 'SRC_TS': 1, 'SRC_GROUP': 1, 'DST-TYPE': 'MASTER', 'DST_NET': 'MASTER-1', 'DST_TS': 2, 'DST_GROUP': 2},
+            {'NAME': 'STATEWIDE', 'DST_NET': 'MASTER-1', 'SRC_TS': 2, 'SRC_GROUP': 3120, 'DST_TS': 2, 'DST_GROUP': 3120, 'ACTIVE': True, 'TO_TYPE': 'ON', 'TIMEOUT': 2, 'ON': [8,], 'OFF': [9,10]},
             # When DMRD received on this CLIENT, Time Slot 1, Talk Group 1; send to MASTER-1 on Time Slot 2 Talk Group 2
             # This rule is NOT enabled by default
             # This rule can be enabled by transmitting on TGID 8
