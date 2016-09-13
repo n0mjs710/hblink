20a2724f3d745f90a5607bd7a55ec8c1780f715b
Cort Buffington <n0mjs@me.com>
2016-09-13 16:21:57 -0500

    Significant Progress

--- bptc19696.py
+++ bptc19696.py
@@ -2,22 +2,66 @@ from __future__ import print_function
 
 from bitstring import BitArray
 from binascii import b2a_hex as h
+from time import time
 
-raw_data = BitArray(196)
-de_interleaved = BitArray(196)
-
-test = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a'
-
-def to_int(_hex_string):
-    return int(h(_hex_string), 16)
-
-def decode(_data):
-    pass
     
-def extract_binary(_data):
-    _data = to_int(_data)
+def binary_196(_data):
+    _data = bytearray(_data)
     _data = BitArray(_data)
-    print(_data)    
+    return _data[:98] + _data[-98:]
+    
+def deint_196(_data):
+    deint = BitArray(196)
+    for index in range(196):
+        deint[index] = _data[(index * 181) % 196]
+    return deint
+    
+def extract_196(_data):
+    databits = BitArray()
+    databits.append(_data[4:12])
+    databits.append(_data[19:30])
+    databits.append(_data[34:45])
+    databits.append(_data[49:60])
+    databits.append(_data[64:75])
+    databits.append(_data[79:90])
+    databits.append(_data[94:105])
+    databits.append(_data[109:120])
+    databits.append(_data[124:135])
+    return databits.tobytes()
 
-print(h(test))
-extract_binary(test)
+
+if __name__ == '__main__':
+    
+    # Validation Example
+    data = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20'
+    
+    t0 = time()
+    bin_data = binary_196(data)
+    t1 = time()
+    deint_data = deint_196(bin_data)
+    t2 = time()
+    ext_data = extract_196(deint_data)
+    t3 = time()
+    
+    print('binary extraction time:', t1-t0)
+    print('deinterleave time:     ', t2-t1)
+    print('byte extraction time:  ',t3-t2,'\n')
+    
+    print('Original 33 byte data block:')
+    print(h(data))
+    print(len(data))
+    print()
+    
+    print('Extracted binary data (distarding sync)')
+    print(bin_data.len)
+    print(bin_data.bin)
+    print()
+
+    print('Deinterleaved binary data')
+    print(deint_data.len)
+    print(deint_data.bin)
+    print()
+
+    print('12 Bytes LC+RS (9,3)')
+    print(len(ext_data))
+    print(h(ext_data))
