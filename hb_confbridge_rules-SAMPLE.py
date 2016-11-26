31cc1f41b58d329bb339764e59a9cc4ac9eb50f6
Cort Buffington <n0mjs@me.com>
2016-11-26 12:10:03 -0600

    NEW CONFERENCE BRIDGE APP!!!
    This is what most of you wanted – and the call router isn’t.

---
+++ hb_confbridge_rules-SAMPLE.py
@@ -0,0 +1,24 @@
+'''
+THIS EXAMPLE WILL NOT WORK AS IT IS - YOU MUST SPECIFY YOUR OWN VALUES!!!
+
+
+'''
+
+BRIDGES = {
+    'WORLDWIDE': [
+            {'SYSTEM': 'MASTER-1',    'TS': 1, 'TGID': 1,    'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'ON',  'ON': [2,], 'OFF': [9,]},
+            {'SYSTEM': 'CLIENT-1',    'TS': 1, 'TGID': 3100, 'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'ON',  'ON': [2,], 'OFF': [9,]},
+        ],
+    'ENGLISH': [
+            {'SYSTEM': 'MASTER-1',    'TS': 1, 'TGID': 13,   'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE', 'ON': [3,], 'OFF': [8,]},
+            {'SYSTEM': 'CLIENT-2',    'TS': 1, 'TGID': 13,   'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE', 'ON': [3,], 'OFF': [8,]},
+        ],
+    'STATEWIDE': [
+            {'SYSTEM': 'MASTER-1',    'TS': 2, 'TGID': 3129, 'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE', 'ON': [4,], 'OFF': [7,]},
+            {'SYSTEM': 'CLIENT-2',    'TS': 2, 'TGID': 3129, 'ACTIVE': True, 'TIMEOUT': 2, 'TO_TYPE': 'NONE', 'ON': [4,], 'OFF': [7,]},
+        ]
+}
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(BRIDGES)
