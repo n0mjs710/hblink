69941711c0cd7cc72ea382487f2ba97439256c20
Cort Buffington <n0mjs@me.com>
2016-12-21 20:58:18 -0600

    Fixed Bug - See Description
    There was missing logic to detect whether or not a target was active.
    This could cause one-way transmission and a system only being 1/2
    disconnected from a conference bridge.

--- hb_confbridge.py
+++ hb_confbridge.py
@@ -254,81 +254,82 @@ class routerSYSTEM(HBSYSTEM):
                     if (_system['SYSTEM'] == self._system and _system['TGID'] == _dst_id and _system['TS'] == _slot and _system['ACTIVE'] == True):
                         
                         for _target in BRIDGES[_bridge]:
-                            if _target['SYSTEM'] != self._system:                                
-                                _target_status = systems[_target['SYSTEM']].STATUS
-                                _target_system = self._CONFIG['SYSTEMS'][_target['SYSTEM']]
+                            if _target['SYSTEM'] != self._system:
+                                if _target['ACTIVE']:                             
+                                    _target_status = systems[_target['SYSTEM']].STATUS
+                                    _target_system = self._CONFIG['SYSTEMS'][_target['SYSTEM']]
                                 
-                                # BEGIN CONTENTION HANDLING
-                                #
-                                # The rules for each of the 4 "ifs" below are listed here for readability. The Frame To Send is:
-                                #   From a different group than last RX from this HBSystem, but it has been less than Group Hangtime
-                                #   From a different group than last TX to this HBSystem, but it has been less than Group Hangtime
-                                #   From the same group as the last RX from this HBSystem, but from a different subscriber, and it has been less than stream timeout
-                                #   From the same group as the last TX to this HBSystem, but from a different subscriber, and it has been less than stream timeout
-                                # The "continue" at the end of each means the next iteration of the for loop that tests for matching rules
-                                #
-                                if ((_target['TGID'] != _target_status[_target['TS']]['RX_TGID']) and ((pkt_time - _target_status[_target['TS']]['RX_TIME']) < _target_system['GROUP_HANGTIME'])):
-                                    if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
-                                        self._logger.info('(%s) Call not routed to TGID %s, target active or in group hangtime: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['RX_TGID']))
-                                    continue
-                                if ((_target['TGID'] != _target_status[_target['TS']]['TX_TGID']) and ((pkt_time - _target_status[_target['TS']]['TX_TIME']) < _target_system['GROUP_HANGTIME'])):
-                                    if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
-                                        self._logger.info('(%s) Call not routed to TGID%s, target in group hangtime: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['TX_TGID']))
-                                    continue
-                                if (_target['TGID'] == _target_status[_target['TS']]['RX_TGID']) and ((pkt_time - _target_status[_target['TS']]['RX_TIME']) < hb_const.STREAM_TO):
-                                    if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
-                                        self._logger.info('(%s) Call not routed to TGID%s, matching call already active on target: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['RX_TGID']))
-                                    continue
-                                if (_target['TGID'] == _target_status[_target['TS']]['TX_TGID']) and (_rf_src != _target_status[_target['TS']]['TX_RFS']) and ((pkt_time - _target_status[_target['TS']]['TX_TIME']) < hb_const.STREAM_TO):
-                                    if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
-                                        self._logger.info('(%s) Call not routed for subscriber %s, call route in progress on target: HBSystem: %s, TS: %s, TGID: %s, SUB: %s', self._system, int_id(_rf_src), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['TX_TGID']), _target_status[_target['TS']]['TX_RFS'])
-                                    continue
+                                    # BEGIN CONTENTION HANDLING
+                                    #
+                                    # The rules for each of the 4 "ifs" below are listed here for readability. The Frame To Send is:
+                                    #   From a different group than last RX from this HBSystem, but it has been less than Group Hangtime
+                                    #   From a different group than last TX to this HBSystem, but it has been less than Group Hangtime
+                                    #   From the same group as the last RX from this HBSystem, but from a different subscriber, and it has been less than stream timeout
+                                    #   From the same group as the last TX to this HBSystem, but from a different subscriber, and it has been less than stream timeout
+                                    # The "continue" at the end of each means the next iteration of the for loop that tests for matching rules
+                                    #
+                                    if ((_target['TGID'] != _target_status[_target['TS']]['RX_TGID']) and ((pkt_time - _target_status[_target['TS']]['RX_TIME']) < _target_system['GROUP_HANGTIME'])):
+                                        if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
+                                            self._logger.info('(%s) Call not routed to TGID %s, target active or in group hangtime: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['RX_TGID']))
+                                        continue
+                                    if ((_target['TGID'] != _target_status[_target['TS']]['TX_TGID']) and ((pkt_time - _target_status[_target['TS']]['TX_TIME']) < _target_system['GROUP_HANGTIME'])):
+                                        if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
+                                            self._logger.info('(%s) Call not routed to TGID%s, target in group hangtime: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['TX_TGID']))
+                                        continue
+                                    if (_target['TGID'] == _target_status[_target['TS']]['RX_TGID']) and ((pkt_time - _target_status[_target['TS']]['RX_TIME']) < hb_const.STREAM_TO):
+                                        if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
+                                            self._logger.info('(%s) Call not routed to TGID%s, matching call already active on target: HBSystem: %s, TS: %s, TGID: %s', self._system, int_id(_target['TGID']), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['RX_TGID']))
+                                        continue
+                                    if (_target['TGID'] == _target_status[_target['TS']]['TX_TGID']) and (_rf_src != _target_status[_target['TS']]['TX_RFS']) and ((pkt_time - _target_status[_target['TS']]['TX_TIME']) < hb_const.STREAM_TO):
+                                        if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD and self.STATUS[_slot]['RX_STREAM_ID'] != _seq:
+                                            self._logger.info('(%s) Call not routed for subscriber %s, call route in progress on target: HBSystem: %s, TS: %s, TGID: %s, SUB: %s', self._system, int_id(_rf_src), _target['SYSTEM'], _target['TS'], int_id(_target_status[_target['TS']]['TX_TGID']), _target_status[_target['TS']]['TX_RFS'])
+                                        continue
                                 
-                                # Set values for the contention handler to test next time there is a frame to forward
-                                _target_status[_target['TS']]['TX_TIME'] = pkt_time
+                                    # Set values for the contention handler to test next time there is a frame to forward
+                                    _target_status[_target['TS']]['TX_TIME'] = pkt_time
                     
-                                if (_stream_id != self.STATUS[_slot]['RX_STREAM_ID']) or (_target_status[_target['TS']]['TX_RFS'] != _rf_src) or (_target_status[_target['TS']]['TX_TGID'] != _target['TGID']):       
-                                    # Record the DST TGID and Stream ID
-                                    _target_status[_target['TS']]['TX_TGID'] = _target['TGID']
-                                    _target_status[_target['TS']]['TX_STREAM_ID'] = _stream_id
-                                    _target_status[_target['TS']]['TX_RFS'] = _rf_src
-                                    # Generate LCs (full and EMB) for the TX stream
-                                    dst_lc = self.STATUS[_slot]['RX_LC'][0:3] + _target['TGID'] + _rf_src
-                                    _target_status[_target['TS']]['TX_H_LC'] = bptc.encode_header_lc(dst_lc)
-                                    _target_status[_target['TS']]['TX_T_LC'] = bptc.encode_terminator_lc(dst_lc)
-                                    _target_status[_target['TS']]['TX_EMB_LC'] = bptc.encode_emblc(dst_lc)
-                                    self._logger.debug('(%s) Generating TX FULL and EMB LCs for destination: System: %s, TS: %s, TGID: %s', self._system, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
-                                    self._logger.info('(%s) Conference Bridge: %s, Call Bridged to: System: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                                    if (_stream_id != self.STATUS[_slot]['RX_STREAM_ID']) or (_target_status[_target['TS']]['TX_RFS'] != _rf_src) or (_target_status[_target['TS']]['TX_TGID'] != _target['TGID']):       
+                                        # Record the DST TGID and Stream ID
+                                        _target_status[_target['TS']]['TX_TGID'] = _target['TGID']
+                                        _target_status[_target['TS']]['TX_STREAM_ID'] = _stream_id
+                                        _target_status[_target['TS']]['TX_RFS'] = _rf_src
+                                        # Generate LCs (full and EMB) for the TX stream
+                                        dst_lc = self.STATUS[_slot]['RX_LC'][0:3] + _target['TGID'] + _rf_src
+                                        _target_status[_target['TS']]['TX_H_LC'] = bptc.encode_header_lc(dst_lc)
+                                        _target_status[_target['TS']]['TX_T_LC'] = bptc.encode_terminator_lc(dst_lc)
+                                        _target_status[_target['TS']]['TX_EMB_LC'] = bptc.encode_emblc(dst_lc)
+                                        self._logger.debug('(%s) Generating TX FULL and EMB LCs for destination: System: %s, TS: %s, TGID: %s', self._system, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                                        self._logger.info('(%s) Conference Bridge: %s, Call Bridged to: System: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
                     
-                                # Handle any necessary re-writes for the destination
-                                if _system['TS'] != _target['TS']:
-                                    _tmp_bits = _bits ^ 1 << 7
-                                else:
-                                    _tmp_bits = _bits
+                                    # Handle any necessary re-writes for the destination
+                                    if _system['TS'] != _target['TS']:
+                                        _tmp_bits = _bits ^ 1 << 7
+                                    else:
+                                        _tmp_bits = _bits
                     
-                                # Assemble transmit HBP packet header
-                                _tmp_data = _data[:8] + _target['TGID'] + _data[11:15] + chr(_tmp_bits) + _data[16:20]
+                                    # Assemble transmit HBP packet header
+                                    _tmp_data = _data[:8] + _target['TGID'] + _data[11:15] + chr(_tmp_bits) + _data[16:20]
                     
-                                # MUST TEST FOR NEW STREAM AND IF SO, RE-WRITE THE LC FOR THE TARGET
-                                # MUST RE-WRITE DESTINATION TGID IF DIFFERENT
-                                # if _dst_id != rule['DST_GROUP']:
-                                dmrbits = bitarray(endian='big')
-                                dmrbits.frombytes(dmrpkt)
-                                # Create a voice header packet (FULL LC)
-                                if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD:
-                                    dmrbits = _target_status[_target['TS']]['TX_H_LC'][0:98] + dmrbits[98:166] + _target_status[_target['TS']]['TX_H_LC'][98:197]
-                                # Create a voice terminator packet (FULL LC)
-                                elif _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VTERM:
-                                    dmrbits = _target_status[_target['TS']]['TX_T_LC'][0:98] + dmrbits[98:166] + _target_status[_target['TS']]['TX_T_LC'][98:197]
-                                # Create a Burst B-E packet (Embedded LC)
-                                elif _dtype_vseq in [1,2,3,4]:
-                                    dmrbits = dmrbits[0:116] + _target_status[_target['TS']]['TX_EMB_LC'][_dtype_vseq] + dmrbits[148:264]
-                                dmrpkt = dmrbits.tobytes()
-                                _tmp_data = _tmp_data + dmrpkt + _data[53:55]
+                                    # MUST TEST FOR NEW STREAM AND IF SO, RE-WRITE THE LC FOR THE TARGET
+                                    # MUST RE-WRITE DESTINATION TGID IF DIFFERENT
+                                    # if _dst_id != rule['DST_GROUP']:
+                                    dmrbits = bitarray(endian='big')
+                                    dmrbits.frombytes(dmrpkt)
+                                    # Create a voice header packet (FULL LC)
+                                    if _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VHEAD:
+                                        dmrbits = _target_status[_target['TS']]['TX_H_LC'][0:98] + dmrbits[98:166] + _target_status[_target['TS']]['TX_H_LC'][98:197]
+                                    # Create a voice terminator packet (FULL LC)
+                                    elif _frame_type == hb_const.HBPF_DATA_SYNC and _dtype_vseq == hb_const.HBPF_SLT_VTERM:
+                                        dmrbits = _target_status[_target['TS']]['TX_T_LC'][0:98] + dmrbits[98:166] + _target_status[_target['TS']]['TX_T_LC'][98:197]
+                                    # Create a Burst B-E packet (Embedded LC)
+                                    elif _dtype_vseq in [1,2,3,4]:
+                                        dmrbits = dmrbits[0:116] + _target_status[_target['TS']]['TX_EMB_LC'][_dtype_vseq] + dmrbits[148:264]
+                                    dmrpkt = dmrbits.tobytes()
+                                    _tmp_data = _tmp_data + dmrpkt + _data[53:55]
                     
-                                # Transmit the packet to the destination system
-                                systems[_target['SYSTEM']].send_system(_tmp_data)
-                                #self._logger.debug('(%s) Packet routed by bridge: %s to system: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                                    # Transmit the packet to the destination system
+                                    systems[_target['SYSTEM']].send_system(_tmp_data)
+                                    #self._logger.debug('(%s) Packet routed by bridge: %s to system: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
             
             
             
