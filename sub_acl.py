6584ba7431ab9297c3c2bb22276cddec8c36a7d2
Cort Buffington <n0mjs@me.com>
2017-06-29 13:02:40 -0500

    update ACL format and action to add "ranges"

--- sub_acl.py
+++ sub_acl.py
@@ -1,12 +1,5 @@
-'''
-This is the Access Control List (ACL) file for limiting call
-routing/bridging in various hblink.py-based applications. It
-is a VERY simple format. The action may be to PERMIT or DENY
-and the ACL itself is a list of subscriber IDs that may be
-permitted or denied.
-'''
-
-ACL_ACTION = "DENY"  # May be PERMIT|DENY
-ACL = [
-    1,2,3,4,5,6,7,8,9,10,100
-    ]
+# The 'action' May be PERMIT|DENY
+# Each entry may be a single radio id, or a hypenated range (e.g. 1-2999)
+# Format:
+# ACL = 'action:id|start-end|,id|start-end,....'
+ACL = 'DENY:0-2999,4000000-9999999'
