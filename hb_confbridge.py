d9dd1881ad6810b38c1f55b1f42d03e9eabecff0
Cort Buffington <n0mjs@me.com>
2018-08-23 14:13:33 -0500

    Fix problem of not starting up when a subscriber ACL is not defined

--- hb_confbridge.py
+++ hb_confbridge.py
@@ -100,13 +100,14 @@ def make_bridges(_hb_confbridge_bridges):
 # Global action is to allow or deny them. Multiple lists with different actions and ranges
 # are not yet implemented.
 def build_acl(_sub_acl):
+    ACL = set()
     try:
-        logger.info('ACL file found, importing entries. This will take about 1.5 seconds per 1 million IDs')
         acl_file = import_module(_sub_acl)
+        logger.info('ACL file found, importing entries. This will take about 1.5 seconds per 1 million IDs')
         sections = acl_file.ACL.split(':')
         ACL_ACTION = sections[0]
         entries_str = sections[1]
-        ACL = set()
+
         
         for entry in entries_str.split(','):
             if '-' in entry:
