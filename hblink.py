f80331cc0facd5501559b616b12de1504eb94e0e
Cort Buffington <n0mjs@me.com>
2018-10-12 15:52:03 -0500

    Fixed hmac calculation typo

--- hblink.py
+++ hblink.py
@@ -219,7 +219,7 @@ class OPENBRIDGE(DatagramProtocol):
         if _packet[:4] == 'DMRD':    # DMRData -- encapsulated DMR data frame
             _data = _packet[:53]
             _hash = _packet[53:]
-            _ckhs = hmac_new(self._config['PASSPHRASE'],_data[53:],sha1).digest()
+            _ckhs = hmac_new(self._config['PASSPHRASE'],_data,sha1).digest()
             
             if compare_digest(_hash, _ckhs) and _sockaddr == self._config['TARGET_SOCK']:
                 _peer_id = _data[11:15]
